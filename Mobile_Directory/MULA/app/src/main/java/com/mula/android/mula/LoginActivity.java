209e8035e7beb0b7bcdbf9cb415312d56ff72bfc
Phat PT <phatpt.dev@gmail.com>
2018-10-14 23:05:35 +0700

    Sign In screen

--- Mobile_Directory/MULA/app/src/main/java/com/mula/android/mula/LoginActivity.java
+++ Mobile_Directory/MULA/app/src/main/java/com/mula/android/mula/LoginActivity.java
@@ -3,7 +3,7 @@ package com.mula.android.mula;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
