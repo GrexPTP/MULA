0a92af3cf65b941394a21195d6c46339a82c3ff1
Phat PT <phatpt.dev@gmail.com>
2018-10-14 23:38:01 +0700

    Sign Up activity create

--- Mobile_Directory/MULA/app/src/main/java/com/mula/android/mula/LoginActivity.java
+++ Mobile_Directory/MULA/app/src/main/java/com/mula/android/mula/LoginActivity.java
@@ -1,13 +1,25 @@
 package com.mula.android.mula;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signUp = findViewById(R.id.sign_up);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
